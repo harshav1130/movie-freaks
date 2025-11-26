160311e10a41bc8b1de92abb18fcca803e0fb6d5
harsha1130 <sri2003harshav@gmail.com>
2025-11-26 15:48:18 +0530

    Updated API URL for Production

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@
 const IS_PRODUCTION = true; 
 
 export const API_URL = IS_PRODUCTION 
-    ? "https://your-backend-name.onrender.com" // We will get this URL in Phase 4
+    ? "https://movie-freaks.onrender.com" // We will get this URL in Phase 4
     : "http://localhost:3001";
